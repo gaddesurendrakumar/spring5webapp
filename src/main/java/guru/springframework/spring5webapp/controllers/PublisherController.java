820584100e03fc709ea6072e9576977fadd23ab5
sgadde <gaddesurendrakumar@gmail.com>
2020-03-04 09:53:13 +0530

    Added publishers and authors thymleaf presenantion

--- src/main/java/guru/springframework/spring5webapp/controllers/PublisherController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/PublisherController.java
@@ -1,4 +1,20 @@
 package guru.springframework.spring5webapp.controllers;
 
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
 public class PublisherController {
+    private final PublisherRepository publisherRepository;
+
+    public PublisherController(PublisherRepository publisherRepository) {
+        this.publisherRepository = publisherRepository;
+    }
+
+    @RequestMapping("/publishers")
+    public String listAuthors(Model model){
+        model.addAttribute("publishers", publisherRepository.findAll());
+        return "publishers";
+    }
 }
